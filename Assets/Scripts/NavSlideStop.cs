e77bd94f04335dfd490d57aac1f79555dad70871
matteovera001 <matteovera001@gmail.com>
2020-07-23 03:17:07 -0500

    Fixed Sliding Issues with NavMesh

---
+++ Assets/Scripts/NavSlideStop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavSlideStop : MonoBehaviour
+{
+    public float acceleration = 2f;
+    public float deceleration = 60f;
+    public float closeEnoughMeters = 4f;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (navMeshAgent)
+        {
+
+            // speed up slowly, but stop quickly
+            if (navMeshAgent.hasPath)
+                navMeshAgent.acceleration = (navMeshAgent.remainingDistance < closeEnoughMeters) ? deceleration : acceleration;
+
+        }
+    }
+
+}
