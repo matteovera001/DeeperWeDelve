855f9b499e303c5c4b14679d7a4ae0239d114341
matteovera001 <matteovera001@gmail.com>
2020-07-23 02:56:47 -0500

    Character2Danim

---
+++ Assets/Scripts/DirAnim.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirAnim : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Sprite upSprite;
+    public Sprite downSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+
+    [Header("Components")]
+    public SpriteRenderer spriteRend;
+    public Transform par;
+    public Animator anim;
+
+    [Header("Calculations")]
+    public float headingAngle;
+    public Vector3 prevLoc;
+    enum Directions {down,left,right,up};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+        prevLoc = transform.position;
+        //par = GetComponentInParent<Transform>();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 curVel = (transform.position - prevLoc) / Time.deltaTime;
+        float mag = (curVel.x * curVel.x) + (curVel.z * curVel.z);
+        prevLoc = transform.position;
+        anim.SetFloat("Velocity", mag);
+
+        Vector3 forward = par.forward;
+        forward.y = 0;
+        headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
+
+        if (!(mag <= .1 && mag >= -.1))
+        {
+            Debug.Log("MOVING " + mag);
+            anim.enabled = true;
+
+            
+            if ((headingAngle <= 45) || (headingAngle >= 315))
+            {
+                anim.SetInteger("Direction", (int)Directions.down);
+            }
+            else if ((headingAngle >= 45) && (headingAngle <= 135))
+            {
+                anim.SetInteger("Direction", (int)Directions.left);
+            }
+            else if ((headingAngle <= 315) && (headingAngle >= 225))
+            {
+                anim.SetInteger("Direction", (int)Directions.right);
+            }
+            else
+            {
+                anim.SetInteger("Direction", (int)Directions.up);
+            }
+
+        }
+        else
+        {
+            anim.enabled = false;
+            Debug.Log("Still");
+            if ((headingAngle <= 45) || (headingAngle >= 315))
+            {
+                spriteRend.sprite = downSprite;
+            }
+            else if ((headingAngle >= 45) && (headingAngle <= 135))
+            {
+                spriteRend.sprite = leftSprite;
+            }
+            else if ((headingAngle <= 315) && (headingAngle >= 225))
+            {
+                spriteRend.sprite = rightSprite;
+            }
+            else
+            {
+                spriteRend.sprite = upSprite;
+            }
+        }
+        
+
+
+        //
+
+        
+
+        
+
+    }
+    
+}
---
+++ Assets/Scripts/FaceNavMeshMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FaceNavMeshMovement : MonoBehaviour
+{
+    //Credit:
+    //https://thehangarter.wordpress.com/2017/05/25/rotating-the-character-with-navmeshagent-navigation/
+    NavMeshAgent agent;
+    Quaternion oldRot;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        transform.rotation = oldRot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (/*!agent.isStopped*/true)
+        {
+            var targetPosition = agent.pathEndPosition;
+            var targetPoint = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
+            var _direction = (targetPoint - transform.position).normalized;
+            var _lookRotation = Quaternion.LookRotation(_direction);
+
+            //OLD
+
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, _lookRotation, 360);
+
+            //New
+            
+
+            Quaternion possibleRot = Quaternion.RotateTowards(transform.rotation, _lookRotation, 360); 
+            if (possibleRot.y==0)
+            {
+                transform.rotation = oldRot;
+            }
+            else
+            {
+                transform.rotation = possibleRot;
+            }
+
+            
+
+            oldRot = transform.rotation;
+            
+        }
+    }
+}
